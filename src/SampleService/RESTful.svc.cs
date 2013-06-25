6950926b9cf1c74c91c5c1fb332bef2eb9aba04f
Adrian Black <superstator@gmail.com>
2013-06-24 22:02:36 -0700

    Added some basic [Hidden] examples to SampleService

--- src/SampleService/RESTful.svc.cs
+++ src/SampleService/RESTful.svc.cs
@@ -9,37 +9,37 @@ using System.Text;
 
 namespace SampleService
 {
-	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-	// NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-	[Swaggerated("/rest", "A RESTful WCF Service")]
-	public class RESTful : IRESTful
-	{
-        [Hidden]
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [Swaggerated("/rest", "A RESTful WCF Service")]
+    public class RESTful : IRESTful
+    {
         public string GetData(string value)
-		{
-			return string.Format("You entered: {0}", value);
-		}
+        {
+            return string.Format("You entered: {0}", value);
+        }
 
-		public CompositeType GetDataUsingDataContract(CompositeType composite)
-		{
-			if (composite == null)
-			{
-				throw new ArgumentNullException("composite");
-			}
-			if (composite.BoolValue)
-			{
-				composite.StringValue += "Suffix";
-			}
-			return composite;
-		}
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+                composite.Secret = new SecretContainer() { SecretData = "Boo!" };
+            }
+            return composite;
+        }
 
-		public CompositeType GetDataUsingDataContract(string composite)
-		{
-			return new CompositeType()
-			{
-				BoolValue = false,
-				StringValue = "foobar"
-			};
-		}
-	}
+        public CompositeType GetDataUsingDataContract(string composite)
+        {
+            return new CompositeType()
+            {
+                BoolValue = false,
+                StringValue = "foobar"
+            };
+        }
+    }
 }
