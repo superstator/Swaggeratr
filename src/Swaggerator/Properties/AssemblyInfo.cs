4a0817f74acb7629c00f233957d037384d5d07bc
Adrian Black <ablack@spiretech.com>
2013-11-04 11:46:39 -0800

    Update AssemblyInfo.cs

--- src/Swaggerator/Properties/AssemblyInfo.cs
+++ src/Swaggerator/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Swaggerator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Digimarc Corporation")]
 [assembly: AssemblyProduct("Swaggerator")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2013.11.1.1")]
-[assembly: AssemblyFileVersion("2013.11.1.1")]
+[assembly: AssemblyVersion("2013.11.4.1")]
+[assembly: AssemblyFileVersion("2013.11.4.1")]
 
 [assembly: InternalsVisibleTo("Swaggerator.Test")]
