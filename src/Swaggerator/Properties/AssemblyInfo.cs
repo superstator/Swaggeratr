d134a7fda3b97c42140b7c8b38dc197975b45b56
makotogitdev <kotokuwa@gmail.com>
2014-05-02 13:53:56 -0700

    Updated version to v2014.5.2.1.

--- src/Swaggerator/Properties/AssemblyInfo.cs
+++ src/Swaggerator/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2014.1.31.1")]
-[assembly: AssemblyFileVersion("2014.1.31.1")]
+[assembly: AssemblyVersion("2014.5.2.1")]
+[assembly: AssemblyFileVersion("2014.5.2.1")]
 
 [assembly: InternalsVisibleTo("Swaggerator.Test")]
